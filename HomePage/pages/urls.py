136b108b272cf7c3bfff5f67d9b9d4eb4932a0c7
dschwarz <dschwarz26@gmail.com>
2014-02-22 01:25:18 +0000

    Added resume page

--- HomePage/pages/urls.py
+++ HomePage/pages/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 from HomePage.pages import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.home, name='home')
+    url(r'^$', views.home, name='home'),
+    url(r'resume/$', views.resume, name='resume')
 )
